b1caf3553856862d92c038937f19b0200c1edd8b
Rafa <andraffal@gmail.com>
2022-02-23 10:56:42 +0300

    Added load and error handler for RandomChar Component

---
+++ src/components/errorMassage/ErrorMassage.js
@@ -0,0 +1,10 @@
+import img from './error.gif'
+
+
+const ErrorMassage = () =>{
+    return(
+        <img src = {img} alt='Error' style={{ display: 'block', width: "250px", height: "250px",objectFit: 'contain', margin: "0 auto"}}></img>
+    )
+}
+
+export default ErrorMassage;
